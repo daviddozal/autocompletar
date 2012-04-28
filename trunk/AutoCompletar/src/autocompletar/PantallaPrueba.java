0f1abc975799d15b74ec6706748b44b6d424093b
dnarce <dnarce@3afa7a9d-d210-f10f-17b1-e940bf5385dc>
2012-04-28 16:45:09 +0000

    Version Mejorada de Autocompletar, ahora se le agrega un modelo, para poder tener la lista de objetos que queramos. Primera version estable

--- trunk/AutoCompletar/src/autocompletar/PantallaPrueba.java
+++ trunk/AutoCompletar/src/autocompletar/PantallaPrueba.java
@@ -28,7 +28,6 @@ public class PantallaPrueba extends JFrame {
         ModeloAutoCompletarDefault modelo = new ModeloAutoCompletarDefault();
         
         ((AutoCompletar)txtCampo).setModelo(modelo);
-        autoCompletarTextField1.setModelo(modelo);
         
         //((AutoCompletar)txtCampo).setLista(possible);
        
@@ -55,11 +54,10 @@ public class PantallaPrueba extends JFrame {
         jTextField1 = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
         txtCampo = new autocompletar.logica.AutoCompletar();
-        jLabel2 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jtextAreaLista = new javax.swing.JTextArea();
         jLabel3 = new javax.swing.JLabel();
-        autoCompletarTextField1 = new autocompletar.componente.AutoCompletarTextField();
+        cabecera = new javax.swing.JLabel();
 
         jTextField1.setText("jTextField1");
 
@@ -68,40 +66,39 @@ public class PantallaPrueba extends JFrame {
 
         jLabel1.setText("Filtro");
 
-        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        jLabel2.setText("Prueba AutoCompletar");
-
         jtextAreaLista.setColumns(20);
         jtextAreaLista.setRows(5);
         jScrollPane1.setViewportView(jtextAreaLista);
 
         jLabel3.setText("La lista de prueba tiene estas palabras:");
 
+        cabecera.setIcon(new javax.swing.ImageIcon(getClass().getResource("/autocompletar/imgs/cabecera.png"))); // NOI18N
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(17, 17, 17)
+                .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(txtCampo)
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(autoCompletarTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel1)
-                            .addComponent(jLabel2)
                             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                                 .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.LEADING)
-                                .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-                        .addGap(0, 54, Short.MAX_VALUE)))
-                .addContainerGap())
+                                .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.LEADING)))
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addGap(17, 17, 17))
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(cabecera, javax.swing.GroupLayout.PREFERRED_SIZE, 420, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel2)
-                .addGap(18, 18, 18)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addComponent(cabecera)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(txtCampo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -109,18 +106,15 @@ public class PantallaPrueba extends JFrame {
                 .addComponent(jLabel3)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 173, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(39, 39, 39)
-                .addComponent(autoCompletarTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(75, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private autocompletar.componente.AutoCompletarTextField autoCompletarTextField1;
+    private javax.swing.JLabel cabecera;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextField jTextField1;
