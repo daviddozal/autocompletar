d6170a61d305215de282f404aff20e6d20ff6adc
dnarce <dnarce@3afa7a9d-d210-f10f-17b1-e940bf5385dc>
2012-10-20 17:12:05 +0000

    remodelando! Entidades

---
+++ trunk/AutoCompletar/src/autocompletar/logica/ElementoAutoCompletar.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package autocompletar.logica;
+
+/**
+ *
+ * @author Diego
+ */
+public interface ElementoAutoCompletar {
+    /**
+     * Implementar este método para retornar el String que se mostrará en el Autocompletar
+     * @return 
+     */
+    public String getNombreElemento();
+}
